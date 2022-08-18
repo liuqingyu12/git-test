923ad5c39fa8124b6833d699f61d9d08d4b969bb
Louis <Louis@163.com>
2022-08-18 19:46:38 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @create 2022/8/18-19:28
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
